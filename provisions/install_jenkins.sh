2c952832dceae61c74a8d0c048b11a12d4c60784
km <mahant.kapil@gmail.com>
2015-08-09 15:19:29 -0700

    with install jenkins

---
+++ provisions/install_jenkins.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo yum install java
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+
+# firewall-cmd --zone=public --add-port=8080/tcp --permanent
+# firewall-cmd --reload
+# sudo /etc/init.d/jenkins restart
+# systemctl restart jenkins.service
+
