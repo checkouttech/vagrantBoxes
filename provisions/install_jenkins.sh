c454f49ec7790f88f5689e151ffce3c65e59e2b5
km <mahant.kapil@gmail.com>
2015-08-09 15:47:24 -0700

    with jenkins

--- provisions/install_jenkins.sh
+++ provisions/install_jenkins.sh
@@ -3,7 +3,7 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum -y  install java
-sudo yum install jenkins
+sudo yum -y  install jenkins
 
 # firewall-cmd --zone=public --add-port=8080/tcp --permanent
 # firewall-cmd --reload
