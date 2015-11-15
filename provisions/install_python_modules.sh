5412d3475306636dbf5f7cb00d937835e4c2f9c6
Kapil Mahant <kmahant@LA-MAC-KMAHANT2.local>
2015-11-14 23:19:06 -0800

    with python modules

---
+++ provisions/install_python_modules.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# install bottle ( can be moved into package ) 
+sudo pip install bottle
+
+# install requests 
+sudo pip install requests
+
+sudo pip install statsd
+
+sudo pip install supervisor
+
+sudo pip install python-memcached
+
+sudo pip install yaml
+
+sudo  pip install pyyaml
+
+sudo  pip install memcache
+
+sudo pip install -U pytest
+
+sudo pip install pytest-sugar
+
+
+
+
---
+++ provisions/install_tools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+sudo yum -y install net-tools
+sudo yum -y install nc
+sudo yum -y install vim
+
+sudo yum -y install rpm-build
+
+
+
