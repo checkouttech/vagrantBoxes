77c58577f83700f6da7698c4b053347d380cc4ee
Kapil Mahant <kmahant@LA-MAC-KMAHANT2.local>
2016-03-29 11:38:25 -0700

    for installing jdk

---
+++ provisions/install_jdk.sh
@@ -0,0 +1,10 @@
+
+
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
+
+
+sudo tar -xzf jdk-8u45-linux-x64.tar.gz -C /opt/
+
+
+
+
