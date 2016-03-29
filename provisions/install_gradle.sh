9633ad17eaed0601d1ce0aba818c1e1bf230a1c6
Kapil Mahant <kmahant@LA-MAC-KMAHANT2.local>
2016-03-29 11:37:47 -0700

    with sudo perm

--- provisions/install_gradle.sh
+++ provisions/install_gradle.sh
@@ -2,8 +2,7 @@
 
 wget https://services.gradle.org/distributions/gradle-2.3-bin.zip
 
-unzip gradle-2.3-bin.zip && rm gradle-*.zip && mv gradle-* ./opt/gradle
-
+unzip gradle-2.3-bin.zip && rm gradle-*.zip && sudo mv gradle-* /opt/gradle
 
 
 
