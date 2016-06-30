8fdfe1fcb6d221e4b9bbaa0a14227169af6ec8d2
kapilmahant <kmahant@LA-MAC-KMAHANT2.local>
2016-06-29 21:57:35 -0700

    with jdk in /opt/ directory

--- provisions/install_jdk.sh
+++ provisions/install_jdk.sh
@@ -1,15 +1,30 @@
 
-# NOT WORKING - failing to set JAVA_HOME
-#wget --quiet --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
+# download java 
+wget --quiet --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
+
+
+export JAVA_HOME=/opt/java/
+sudo mkdir $JAVA_HOME
+
+sudo tar -zxvf jdk-8u45-linux-x64.tar.gz   --strip-components 1  -C $JAVA_HOME
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+#write to environment file for all future sessions 
+sudo /bin/sh -c 'echo export JAVA_HOME="/opt/java/" >> /etc/environment'
+sudo /bin/sh -c 'echo export PATH="$JAVA_HOME/bin:$PATH" >> /etc/environment'
+
 
 #sudo tar -xzf jdk-8u45-linux-x64.tar.gz -C /opt/
-
 # export JAVA_HOME=/opt/jdk1.8.0_45
-# export PATH=$JAVA_HOME/bin:$PATH
+
+
+
+
 
 
 # working version 
-sudo yum -y  install java
+#sudo yum -y  install java
 
 
 
