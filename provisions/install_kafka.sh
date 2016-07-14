204667dbe80643c9d6bc9e3b71ad16644552955c
kapilmahant <kmahant@LA-MAC-KMAHANT2.local>
2016-07-13 17:59:21 -0700

    with examples

--- provisions/install_kafka.sh
+++ provisions/install_kafka.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# download kafka 
+# download kafka
 wget --quiet http://apache.claz.org/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz
 
 export KAFKA_HOME=/opt/kafka/
 export KAFKA=$KAFKA_HOME/bin
 export KAFKA_CONFIG=$KAFKA_HOME/config
+
+#write to environment file for all future sessions
+sudo /bin/sh -c 'echo export KAFKA_HOME="/opt/kafka/" >> /etc/environment'
+sudo /bin/sh -c 'echo export KAFKA="$KAFKA_HOME/bin" >> /etc/environment'
+sudo /bin/sh -c 'echo export KAFKA_CONF="$KAFKA_HOME/config" >> /etc/environment'
+
 
 #sudo mkdir /opt/kafka
 sudo mkdir $KAFKA_HOME
@@ -17,7 +23,7 @@ sudo  tar -zxvf kafka_2.11-0.10.0.0.tgz --strip-components 1  -C $KAFKA_HOME
 ############# broker 1 specific settings
 ####################################################
 
-## copy standard issued server properties file as template for broker 1  
+## copy standard issued server properties file as template for broker 1
 sudo cp $KAFKA_CONFIG/server.properties  $KAFKA_CONFIG/server_1.properties
 
 ## set broker ID
@@ -32,7 +38,7 @@ export REPLACEMENT_VALUE=PLAINTEXT:\\/\\/192.168.150.80:9092
 
 sudo sed -c -i "s/#\($TARGET_KEY*=*\).*/\1$REPLACEMENT_VALUE/" $KAFKA_CONFIG/server_1.properties
 
-## set advertised.listener to accept message from remote servers 
+## set advertised.listener to accept message from remote servers
 export TARGET_KEY=advertised.listeners
 export REPLACEMENT_VALUE=PLAINTEXT:\\/\\/192.168.150.80:9092
 
@@ -56,7 +62,7 @@ sudo sed -c -i "s/\($TARGET_KEY=\).*/\1$REPLACEMENT_VALUE/"   $KAFKA_CONFIG/serv
 ####################################################
 
 
-## copy standard issued server properties file as template for broker 2 
+## copy standard issued server properties file as template for broker 2
 sudo cp $KAFKA_CONFIG/server.properties  $KAFKA_CONFIG/server_2.properties
 
 
@@ -72,7 +78,7 @@ export REPLACEMENT_VALUE=PLAINTEXT:\\/\\/192.168.150.80:9093
 
 sudo sed -c -i "s/#\($TARGET_KEY*=*\).*/\1$REPLACEMENT_VALUE/" $KAFKA_CONFIG/server_2.properties
 
-## set advertised.listener to accept message from remote servers 
+## set advertised.listener to accept message from remote servers
 export TARGET_KEY=advertised.listeners
 export REPLACEMENT_VALUE=PLAINTEXT:\\/\\/192.168.150.80:9093
 
@@ -93,14 +99,14 @@ sudo sed -c -i "s/\($TARGET_KEY=\).*/\1$REPLACEMENT_VALUE/"   $KAFKA_CONFIG/serv
 
 
 ####################################################
-#### Restrict Java heap space to avoid overflowing limited free RAM 
-#### Else not all broker will get initialized 
+#### Restrict Java heap space to avoid overflowing limited free RAM
+#### Else not all broker will get initialized
 ####################################################
 
-#write to environment file for all future sessions 
+#write to environment file for all future sessions
 sudo /bin/sh -c 'echo export KAFKA_HEAP_OPTS="-Xmx256M -Xms128M" >> /etc/environment'
 
-#export for the current session before starting kafka cluster 
+#export for the current session before starting kafka cluster
 export KAFKA_HEAP_OPTS="-Xmx256M -Xms128M"
 
 
@@ -112,7 +118,21 @@ sudo $KAFKA/kafka-server-start.sh  -daemon  $KAFKA_CONFIG/server_1.properties
 sudo $KAFKA/kafka-server-start.sh  -daemon  $KAFKA_CONFIG/server_2.properties
 
 
+####################################################
+######## examples
+####################################################
 
 
+# create topic
+#
+#     # /opt/kafka/bin/kafka-topics.sh --create --zookeeper  192.168.150.70:2181  --replication-factor 2 --partitions 2 --topic  my-topic3
+#
+# built in producer
+#
+#    /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic  my-topic3
+#
+# built in consumer
+#
+#   /opt/kafka/bin/kafka-console-consumer.sh --zookeeper 192.168.150.70:2181 --topic my-topic3  --from-beginning
 
 
